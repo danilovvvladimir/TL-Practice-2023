60724f4fc537b8027f822d998351036a2d868b38
Vladimir <fruityflunt@gmail.com>
2023-07-03 16:49:42 +0300

    initial commit; homework1: bubble sort & read info;

---
+++ hw-1/BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,54 @@
+﻿class Program
+{
+    public static void BubbleSort(List<int> lst)
+    {
+        int listSize = lst.Count;
+        bool isAlreadySorted;
+
+        for (int i = 0; i < listSize - 1; i++)
+        {
+            isAlreadySorted = true;
+
+            for (int j = 0; j < listSize - i - 1; j++)
+            {
+                if (lst[j] > lst[j + 1])
+                {
+                    int temp = lst[j];
+
+                    lst[j] = lst[j + 1];
+                    lst[j + 1] = temp;
+
+                    isAlreadySorted = false;
+                }
+            }
+            if (isAlreadySorted)
+            {
+                break;
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        List<int> randomCase = new List<int>()
+        {
+            1, 50, 25,0, -24, 75, 100, 110
+        };
+
+        List<int> worstCase = new List<int>()
+        {
+            110, 100, 75, 50, 25, 1, 0, -24
+        };
+
+        List<int> bestCase = new List<int>()
+        {
+            -24, 0, 1, 25, 50, 75, 100, 110
+        };
+
+
+        BubbleSort(randomCase);
+
+        randomCase.ForEach(number => Console.WriteLine(number));
+    }
+}
+
---
+++ hw-1/ReadInfo/ReadInfo/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // === FIO ===
+        Console.WriteLine("1. Получение имени.");
+        Console.Write("- Введите фамилию: ");
+        string lastName = Console.ReadLine();
+        Console.Write("- Введите имя: ");
+        string firstName = Console.ReadLine();
+        Console.Write("- Введите отчество: ");
+        string middleName = Console.ReadLine();
+        Console.WriteLine();
+
+
+        // === AGE ===
+        Console.WriteLine("2. Получение возраста.");
+        int age = 0;
+        bool isNumber = false;
+
+        while (!isNumber)
+        {
+            Console.Write("Введите число: ");
+            string rowAgeString = Console.ReadLine();
+
+            isNumber = int.TryParse(rowAgeString, out age);
+
+            if (!isNumber)
+            {
+                Console.WriteLine("Ошибка. Возраст должен быть числом. Попробуйте ещё раз.");
+            }
+        }
+        Console.WriteLine();
+
+
+        // === EMAIL ===
+        Console.WriteLine("3. Получение email.");
+        string email = string.Empty;
+        bool isValidEmail = false;
+        Regex emailRegex = new Regex(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$");
+
+        while (!isValidEmail)
+        {
+            Console.Write("Введите email: ");
+            email = Console.ReadLine();
+
+            isValidEmail = emailRegex.IsMatch(email);
+
+            if (!isValidEmail)
+            {
+                Console.WriteLine("Ошибка. Невалидный email.");
+            }
+        }
+        Console.WriteLine();
+
+        // === GITHUB ===
+        Console.WriteLine("4. Получение Github.");
+        Console.Write("Введите github: ");
+        string github = Console.ReadLine();
+        Console.WriteLine();
+
+        // === OUTPUT ===
+        Console.WriteLine("===== INFO =====");
+        Console.WriteLine($"ФИО: \t\t{lastName} {firstName} {middleName}");
+        Console.WriteLine($"Возраст: \t{age}");
+        Console.WriteLine($"Email: \t\t{email}");
+        Console.WriteLine($"Github: \t{github}");
+    }
+}
