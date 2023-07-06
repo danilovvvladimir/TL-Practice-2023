750699461bc2a3fe1ff13a78f78aee5077df89be
Vladimir <fruityflunt@gmail.com>
2023-07-06 15:08:30 +0300

    Исправил issues в 1.1 BubbleSort

--- hw-1/BubbleSort/BubbleSort/Program.cs
+++ hw-1/BubbleSort/BubbleSort/Program.cs
@@ -1,54 +1,56 @@
-﻿class Program
+﻿namespace BubbleSort
 {
-    public static void BubbleSort(List<int> lst)
+    internal class Program
     {
-        int listSize = lst.Count;
-        bool isAlreadySorted;
-
-        for (int i = 0; i < listSize - 1; i++)
+        public static void BubbleSort(List<int> list)
         {
-            isAlreadySorted = true;
+            int listSize = list.Count;
+            bool isAlreadySorted;
 
-            for (int j = 0; j < listSize - i - 1; j++)
+            for (int i = 0; i < listSize - 1; i++)
             {
-                if (lst[j] > lst[j + 1])
+                isAlreadySorted = true;
+
+                for (int j = 0; j < listSize - i - 1; j++)
                 {
-                    int temp = lst[j];
+                    if (list[j] > list[j + 1])
+                    {
+                        int temp = list[j];
 
-                    lst[j] = lst[j + 1];
-                    lst[j + 1] = temp;
+                        list[j] = list[j + 1];
+                        list[j + 1] = temp;
 
-                    isAlreadySorted = false;
+                        isAlreadySorted = false;
+                    }
+                }
+
+                if (isAlreadySorted)
+                {
+                    break;
                 }
             }
-            if (isAlreadySorted)
+        }
+
+        public static void Main(string[] args)
+        {
+            List<int> randomCase = new List<int>()
             {
-                break;
-            }
+                1, 50, 25,0, -24, 75, 100, 110
+            };
+
+            List<int> worstCase = new List<int>()
+            {
+                110, 100, 75, 50, 25, 1, 0, -24
+            };
+
+            List<int> bestCase = new List<int>()
+            {
+                -24, 0, 1, 25, 50, 75, 100, 110
+            };
+
+            BubbleSort(randomCase);
+
+            randomCase.ForEach(number => Console.WriteLine(number));
         }
     }
-
-    static void Main(string[] args)
-    {
-        List<int> randomCase = new List<int>()
-        {
-            1, 50, 25,0, -24, 75, 100, 110
-        };
-
-        List<int> worstCase = new List<int>()
-        {
-            110, 100, 75, 50, 25, 1, 0, -24
-        };
-
-        List<int> bestCase = new List<int>()
-        {
-            -24, 0, 1, 25, 50, 75, 100, 110
-        };
-
-
-        BubbleSort(randomCase);
-
-        randomCase.ForEach(number => Console.WriteLine(number));
-    }
 }
-
