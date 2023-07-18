55ba19502d3aa538ee8f1619461937f09b63f68e
Vladimir <fruityflunt@gmail.com>
2023-07-18 09:18:00 +0300

    Сделал основу 5 ДЗ: форму и страницу с ответом. Осталась валидация, столбики, extra для столбиков, readme

---
+++ hw-5/Calculator/Calculator/Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Calculator.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                double firstNumber = double.Parse(Request.Form["firstNumber"]);
+                double secondNumber = double.Parse(Request.Form["secondNumber"]);
+                string operation = Request.Form["operation"];
+
+                double result = CalculateResult(firstNumber, secondNumber, operation);
+
+                return RedirectToPage("/Result", new { FirstNumber = firstNumber, SecondNumber = secondNumber, Operation = operation, Result = result });
+            }
+            return Page();
+        }
+
+        private double CalculateResult(double firstNumber, double secondNumber, string operation)
+        {
+            switch (operation)
+            {
+                case "+":
+                    return firstNumber + secondNumber;
+                case "-":
+                    return firstNumber - secondNumber;
+                case "*":
+                    return firstNumber * secondNumber;
+                case "/":
+                    return firstNumber / secondNumber;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
---
+++ hw-5/Calculator/Calculator/Pages/Result.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Calculator.Pages
+{
+    public class ResultModel : PageModel
+    {
+        public double FirstNumber { get; set; }
+        public double SecondNumber { get; set; }
+        public string Operation { get; set; }
+        public double Result { get; set; }
+
+        public void OnGet(double firstNumber, double secondNumber, string operation, double result)
+        {
+            FirstNumber = firstNumber;
+            SecondNumber = secondNumber;
+            Operation = operation;
+            Result = result;
+        }
+    }
+}
