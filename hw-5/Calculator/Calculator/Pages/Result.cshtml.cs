0bf093e3d2e2c67c0c69dd2f340473acc574a78b
Vladimir <fruityflunt@gmail.com>
2023-07-18 17:48:29 +0300

    Доделал отображение extra штук, закомментил пока что * и /

--- hw-5/Calculator/Calculator/Pages/Result.cshtml.cs
+++ hw-5/Calculator/Calculator/Pages/Result.cshtml.cs
@@ -10,32 +10,52 @@ namespace Calculator.Pages
         public string Operation { get; set; }
 
         // Calculations:
-        public bool IsMinus { get; set; }
         public int Result { get; set; }
         public List<int> FirstNumberDigits { get; set; }
         public List<int> SecondNumberDigits { get; set; }
         public List<int> ResultDigits { get; set; }
+        public bool IsMinus { get; set; } = false;
+
+        // +
         public List<int> PlusIndexes { get; set; }
+
+        // -
+        public List<int> DotsIndexes { get; set; }
+        public List<int> TensIndexes { get; set; }
 
         public void OnGet(int firstNumber, int secondNumber, string operation)
         {
             Operation = operation;
 
-            FirstNumber = firstNumber;
-            SecondNumber = secondNumber;
-            Result = CalculateResult(FirstNumber, SecondNumber, Operation);
-
-            if (Result < 0)
+            if (Operation == "-" && (firstNumber < secondNumber))
             {
+                FirstNumber = secondNumber;
+                SecondNumber = firstNumber;
                 IsMinus = true;
-                Result *= -1;
+
+                Result = -1 * CalculateResult(SecondNumber, FirstNumber, Operation);
+
+            }
+            else
+            {
+                FirstNumber = firstNumber;
+                SecondNumber = secondNumber;
+                Result = CalculateResult(FirstNumber, SecondNumber, Operation);
             }
 
             FirstNumberDigits = GetDigits(FirstNumber);
             SecondNumberDigits = GetDigits(SecondNumber);
             ResultDigits = GetDigits(Result);
 
-            PlusIndexes = GetPlusIndexes();
+            if (Operation == "+")
+            {
+                PlusIndexes = GetPlusIndexes();
+            }
+
+            if (Operation == "-")
+            {
+                (DotsIndexes, TensIndexes) = GetMinusIndexes();
+            }
 
         }
 
@@ -47,10 +67,10 @@ namespace Calculator.Pages
                     return firstNumber + secondNumber;
                 case "-":
                     return firstNumber - secondNumber;
-                case "*":
-                    return firstNumber * secondNumber;
-                case "/":
-                    return firstNumber / secondNumber;
+                //case "*":
+                //    return firstNumber * secondNumber;
+                //case "/":
+                //    return firstNumber / secondNumber;
                 default:
                     return 0;
             }
@@ -104,6 +124,46 @@ namespace Calculator.Pages
             }
 
             return indexes;
+        }
+
+        public (List<int>, List<int>) GetMinusIndexes()
+        {
+            List<int> DotsIndexes = new List<int>();
+            List<int> TensIndexes = new List<int>();
+
+            List<int> FirstNumberDigitsReversed = new List<int>(FirstNumberDigits);
+            FirstNumberDigitsReversed.Reverse();
+
+            List<int> SecondNumberDigitsReversed = new List<int>(SecondNumberDigits);
+            SecondNumberDigitsReversed.Reverse();
+
+            int maxSize = Math.Max(FirstNumberDigits.Count, SecondNumberDigits.Count);
+            int minSize = Math.Min(FirstNumberDigits.Count, SecondNumberDigits.Count);
+
+            for (int i = 0; i < maxSize; i++)
+            {
+                if (i < minSize)
+                {
+                    if (FirstNumberDigitsReversed[i] - SecondNumberDigitsReversed[i] < 0)
+                    {
+                        DotsIndexes.Add(maxSize - 2 - i);
+                        TensIndexes.Add(maxSize - 1 - i);
+                    }
+                }
+                else
+                {
+                    if (DotsIndexes.Count != 0 && (DotsIndexes[DotsIndexes.Count - 1] == maxSize - i - 1))
+                    {
+                        if (FirstNumberDigitsReversed[i] - 1 < 0)
+                        {
+                            DotsIndexes.Add(maxSize - 2 - i);
+                            TensIndexes.Add(maxSize - 1 - i);
+                        }
+                    }
+                }
+            }
+
+            return (DotsIndexes, TensIndexes);
         }
     }
 }
