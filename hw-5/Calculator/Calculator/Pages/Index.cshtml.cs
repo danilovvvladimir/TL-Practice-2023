f1b1b12deda9911ee8512efe4941b17c07b7bb98
Vladimir <fruityflunt@gmail.com>
2023-07-18 12:04:32 +0300

    Красивое оформление решения столбиком; Функционал сложения и вычитания; Некст - умножение, деление, extra (. и 10);

--- hw-5/Calculator/Calculator/Pages/Index.cshtml.cs
+++ hw-5/Calculator/Calculator/Pages/Index.cshtml.cs
@@ -13,28 +13,9 @@ namespace Calculator.Pages
                 double secondNumber = double.Parse(Request.Form["secondNumber"]);
                 string operation = Request.Form["operation"];
 
-                double result = CalculateResult(firstNumber, secondNumber, operation);
-
-                return RedirectToPage("/Result", new { FirstNumber = firstNumber, SecondNumber = secondNumber, Operation = operation, Result = result });
+                return RedirectToPage("/Result", new { FirstNumber = firstNumber, SecondNumber = secondNumber, Operation = operation });
             }
             return Page();
-        }
-
-        private double CalculateResult(double firstNumber, double secondNumber, string operation)
-        {
-            switch (operation)
-            {
-                case "+":
-                    return firstNumber + secondNumber;
-                case "-":
-                    return firstNumber - secondNumber;
-                case "*":
-                    return firstNumber * secondNumber;
-                case "/":
-                    return firstNumber / secondNumber;
-                default:
-                    return 0;
-            }
         }
     }
 }
--- hw-5/Calculator/Calculator/Pages/Result.cshtml.cs
+++ hw-5/Calculator/Calculator/Pages/Result.cshtml.cs
@@ -4,17 +4,81 @@ namespace Calculator.Pages
 {
     public class ResultModel : PageModel
     {
-        public double FirstNumber { get; set; }
-        public double SecondNumber { get; set; }
+        // From Query:
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
         public string Operation { get; set; }
-        public double Result { get; set; }
 
-        public void OnGet(double firstNumber, double secondNumber, string operation, double result)
+        // Calculations:
+        public bool IsMinus { get; set; }
+        public int Result { get; set; }
+        public List<int> FirstNumberDigits { get; set; }
+        public List<int> SecondNumberDigits { get; set; }
+        public List<int> ResultDigits { get; set; }
+
+        public void OnGet(int firstNumber, int secondNumber, string operation)
         {
+            Operation = operation;
+
+            //if (Operation == "-")
+            //{
+            //    FirstNumber = firstNumber > secondNumber ? firstNumber : secondNumber;
+            //    SecondNumber = secondNumber < firstNumber ? secondNumber : firstNumber;
+            //    Result = CalculateResult(FirstNumber, SecondNumber, Operation);
+            //    IsMinus = true;
+            //}
+            //else
+            //{
+            //    FirstNumber = firstNumber;
+            //    SecondNumber = secondNumber;
+            //    Result = CalculateResult(FirstNumber, SecondNumber, Operation);
+            //}
+
             FirstNumber = firstNumber;
             SecondNumber = secondNumber;
-            Operation = operation;
-            Result = result;
+            Result = CalculateResult(FirstNumber, SecondNumber, Operation);
+
+            if (Result < 0)
+            {
+                IsMinus = true;
+                Result *= -1;
+            }
+
+            FirstNumberDigits = GetDigits(FirstNumber);
+            SecondNumberDigits = GetDigits(SecondNumber);
+            ResultDigits = GetDigits(Result);
+
         }
+
+        private int CalculateResult(int firstNumber, int secondNumber, string operation)
+        {
+            switch (operation)
+            {
+                case "+":
+                    return firstNumber + secondNumber;
+                case "-":
+                    return firstNumber - secondNumber;
+                case "*":
+                    return firstNumber * secondNumber;
+                case "/":
+                    return firstNumber / secondNumber;
+                default:
+                    return 0;
+            }
+        }
+
+        public List<int> GetDigits(int number)
+        {
+            List<int> digits = new List<int>();
+            while (number != 0)
+            {
+                digits.Add(number % 10);
+                number /= 10;
+            }
+            digits.Reverse();
+            return digits;
+        }
+
+
     }
 }
